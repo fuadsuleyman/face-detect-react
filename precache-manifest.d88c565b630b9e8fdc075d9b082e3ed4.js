e23e3daafffcbe533366e2030342aed335ef374f
fuad_suleymanov <fuadsuleymanov@outlook.com>
2020-04-10 22:41:43 +0400

    Updates

---
+++ precache-manifest.d88c565b630b9e8fdc075d9b082e3ed4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53b7b8270a4a15cabc69843297dcd022",
+    "url": "/face-detect-react/index.html"
+  },
+  {
+    "revision": "723443892b99caf0a4ea",
+    "url": "/face-detect-react/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "92eac3c0d21e7aced952",
+    "url": "/face-detect-react/static/css/main.7dc27ae6.chunk.css"
+  },
+  {
+    "revision": "723443892b99caf0a4ea",
+    "url": "/face-detect-react/static/js/2.829da556.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-detect-react/static/js/2.829da556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92eac3c0d21e7aced952",
+    "url": "/face-detect-react/static/js/main.40d55a8f.chunk.js"
+  },
+  {
+    "revision": "e5423a0e40a00887cf25",
+    "url": "/face-detect-react/static/js/runtime-main.94774ec5.js"
+  }
+]);
